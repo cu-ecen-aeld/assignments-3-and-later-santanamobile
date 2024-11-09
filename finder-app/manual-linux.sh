81f74d84165d69fbe2152da503caa62e38efe5af
Helder Santana <3494526+santanamobile@users.noreply.github.com>
2024-11-09 16:49:13 -0300

    commit to force github action to run

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -12,8 +12,10 @@ BUSYBOX_VERSION=1_33_1
 FINDER_APP_DIR=$(realpath $(dirname $0))
 ARCH=arm64
 CROSS_COMPILE=aarch64-none-linux-gnu-
-TOOLCHAIN_LOCATION="/usr/local/arm-cross-compiler/install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc"
+#TOOLCHAIN_LOCATION="/usr/local/arm-cross-compiler/install/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc"
 #TOOLCHAIN_LOCATION="/opt/arm-gnu-toolchain-13.3.rel1-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc/"
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+
 if [ $# -lt 1 ]
 then
 	echo "Using default directory ${OUTDIR} for output"
